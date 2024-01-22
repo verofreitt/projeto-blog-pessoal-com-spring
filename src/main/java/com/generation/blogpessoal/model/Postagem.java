a1f1fd498756c9140dc79da8757e90ebd3c555dd
Verônica Freitas Santos <verofreit@gmail.com>
2024-01-22 11:47:50 -0300

    parte de segurança finalizada

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -36,6 +36,10 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -77,6 +81,13 @@ public class Postagem {
 		this.tema = tema;
 	}
 	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 	
 	
 }
